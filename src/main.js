bdb9318090bc83466a1502edc536083c7e1732b0
mellolo <21921066@zju.edu.cn>
2025-07-21 23:21:43 +0800

    add video player

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+//import '@/style.css'
 import router from '@/router/router'
-import App from '@/sites/App.vue'
+import App from '@/App.vue'
 
 const app = createApp(App)
 app.use(router)
