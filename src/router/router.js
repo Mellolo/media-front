89201c9c4865e9d54bb0269019e3d0c58ac4346d
mellolo <21921066@zju.edu.cn>
2025-07-25 00:00:06 +0800

    update

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import VideoPlayerPage from '@/sites/VideoPlayerPage.vue'
+import VideoPlayerPage from '@/views/VideoPlayerPage.vue'
 
 const routes = [
   {
