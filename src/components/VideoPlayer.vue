3554cb611d0b558dd5aedbbdd01ce6c5849c3054
mellolo <21921066@zju.edu.cn>
2025-08-12 13:15:30 +0800

    视频播放

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -56,8 +56,11 @@ export default {
     this.player = videojs(this.$refs.videoPlayer, {
       controls: true,
       preload: 'auto',
-      fluid: true,
-      responsive: true
+      fluid: false,
+      responsive: false,
+      aspectRatio: '16:9',
+      width: '100%',
+      height: '100%'
     });
   },
   beforeUnmount() {
@@ -71,15 +74,25 @@ export default {
 
 <style scoped>
 .video-player {
-  width: 700px;
+  width: 800px;
   max-width: 1200px;
-  height: 675px;
+  height: 450px;
   margin: 0 auto;
   position: relative;
   overflow: hidden;
+  background-color: #000;
 }
 
 :deep(.video-js) {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+  background-color: #000;
+}
+
+:deep(.vjs-tech) {
   position: absolute;
   top: 0;
   left: 0;
@@ -89,11 +102,14 @@ export default {
 
 :deep(.vjs-default-skin) {
   color: #fff;
+  font-family: Arial, sans-serif;
 }
 
 /* 覆盖默认样式以适应我们的设计 */
 :deep(.vjs-control-bar) {
   background: rgba(0, 0, 0, 0.7);
+  color: #fff;
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
 }
 
 :deep(.vjs-play-progress) {
@@ -102,6 +118,58 @@ export default {
 
 :deep(.vjs-volume-level) {
   background-color: #43d6b4;
+}
+
+:deep(.vjs-slider-bar) {
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+/* 大播放按钮样式 */
+:deep(.vjs-big-play-button) {
+  background-color: rgba(0, 0, 0, 0.5);
+  border: 2px solid #43d6b4;
+  border-radius: 50%;
+  width: 3em;
+  height: 3em;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: all 0.3s ease;
+}
+
+:deep(.vjs-big-play-button:hover) {
+  background-color: rgba(67, 214, 180, 0.5);
+  border-color: #fff;
+}
+
+:deep(.vjs-big-play-button:before) {
+  font-size: 2em;
+  line-height: 1.5em;
+  text-shadow: none;
+}
+
+/* 控制按钮样式 */
+:deep(.vjs-control-bar .vjs-button) {
+  color: #fff;
+}
+
+:deep(.vjs-control-bar .vjs-button:hover) {
+  color: #43d6b4;
+}
+
+/* 时间显示样式 */
+:deep(.vjs-current-time, .vjs-duration) {
+  color: #fff;
+}
+
+/* 全屏按钮样式 */
+:deep(.vjs-fullscreen-control) {
+  float: right;
+}
+
+/* 音量控制样式 */
+:deep(.vjs-volume-panel) {
+  float: right;
 }
 
 /* 响应式控制栏 */
@@ -124,6 +192,16 @@ export default {
 @media (max-width: 768px) {
   :deep(.vjs-control-bar) {
     font-size: 10px;
+  }
+  
+  :deep(.vjs-big-play-button) {
+    width: 2.5em;
+    height: 2.5em;
+  }
+  
+  :deep(.vjs-big-play-button:before) {
+    font-size: 1.5em;
+    line-height: 1.6em;
   }
 }
 </style>
