fdb2c564c5a9d9f40dd5daf546389e42dad39067
mellolo <21921066@zju.edu.cn>
2025-08-20 19:46:17 +0800

    conf

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // 设置 @ 指向 src 目录
-    }
-  },
-  server: {
-    host: '0.0.0.0', // 允许外部IP访问
-    port: 9980,
-    proxy: {
-      '/api': {
-        target: 'http://192.168.5.178:18080/api',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // 设置 @ 指向 src 目录
       }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    optimizeDeps: {
+      include: [
+        'video.js',
+      ]
     }
-  },
-  optimizeDeps: {
-    include: [
-      'video.js',
-    ]
   }
 })
